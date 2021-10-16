376b6b127d7896a1a23470855751dcea20332ad7
1002304490@qq.com <1002304490@qq.com>
2021-10-16 13:22:53 +0800

    登录界面

--- app/src/main/java/com/gao/blueblue/ui/activity/base/BaseActivity.java
+++ app/src/main/java/com/gao/blueblue/ui/activity/base/BaseActivity.java
@@ -17,8 +17,6 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_base);
-
         mContext = this;
 
     }
